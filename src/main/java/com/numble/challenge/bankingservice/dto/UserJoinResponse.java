99942104c1db63b64f3120f99bb1fde7dc1f5c71
Qkite <yeonjigu4@gmail.com>
2023-02-16 19:59:41 +0900

    feat: 회원가입 서비스 - 개발 진행중
    - UserEntity, Dto 생성

--- src/main/java/com/numble/challenge/bankingservice/dto/UserJoinResponse.java
+++ src/main/java/com/numble/challenge/bankingservice/dto/UserJoinResponse.java
@@ -1,18 +1,17 @@
 package com.numble.challenge.bankingservice.dto;
 
-
+import jdk.jfr.Name;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-public class UserJoinRequest {
-
+@AllArgsConstructor
+@Builder
+public class UserJoinResponse {
     private String loginId;
     private String nickName;
-    private String password;
-    private String confirmPassword;
 
 }
